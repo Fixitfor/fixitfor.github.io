7ec9cc38f53cb993fe1604381947b8338380dd0d
PhearZero <github@phearzero.com>
2020-07-08 17:34:47 -0500

    :rocker: Launch

---
+++ precache-manifest.e40756486d396274d40f5c8ec97b2563.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6afa703afeb64e6931",
+    "url": "css/chunk-vendors.0c4eacdb.css"
+  },
+  {
+    "revision": "995cf55911addab4c12f825a1d377391",
+    "url": "img/logo.png"
+  },
+  {
+    "revision": "411194af3570352fe1c897ac84c83704",
+    "url": "img/logo.svg"
+  },
+  {
+    "revision": "c8a05b14a9e536d96908859daa4cc34f",
+    "url": "img/logo_inverted.svg"
+  },
+  {
+    "revision": "be6da73b3dd557d943e2710e7a08d9e4",
+    "url": "img/logo_inverted_blue.svg"
+  },
+  {
+    "revision": "3e2269d24ccf616702ba24b315acc803",
+    "url": "index.html"
+  },
+  {
+    "revision": "6bb098673b326faa5341",
+    "url": "js/app.72786a3e.js"
+  },
+  {
+    "revision": "ea6afa703afeb64e6931",
+    "url": "js/chunk-vendors.3f85617f.js"
+  },
+  {
+    "revision": "9af4670b79a34748012f7638086d9c10",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
